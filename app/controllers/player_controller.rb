f37c00d7d750a69fbbabefd46e8b111fc5252183
cycorld <charleschoi87@gmail.com>
2015-09-08 18:49:53 +0900

    index action

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,4 +1,9 @@
 class PlayerController < ApplicationController
   def index
+    if List.where(id: params[:id]).take
+      @list = List.find(params[:id])
+    else
+      @list = List.last
+    end
   end
 end
